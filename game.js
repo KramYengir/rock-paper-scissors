80b40a90f42fdb21b198c4d938bd28e4d265ec4f
KramYengir <kramyengir@gmail.com>
2023-04-24 15:10:31 +0100

    Make Return Strings Lowercase

--- game.js
+++ game.js
@@ -1,10 +1,12 @@
+
+
 function getComputerChoice(){
     let randomNum = Math.random();
 
     if(randomNum < 0.33)
-        return "Rock";
+        return "rock";
     else if(randomNum < 0.66)
-        return "Paper";
+        return "paper";
 
-    return "Scissors";
+    return "scissors";
 }
