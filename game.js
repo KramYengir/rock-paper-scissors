d6bbee910e851f52f58d5356261f4238888ad30d
KramYengir <kramyengir@gmail.com>
2023-04-24 15:43:09 +0100

    Add "playRound" Function
    This function has a switch statement whic compares the player's
    selection to the computer's and returns the result as a message

--- game.js
+++ game.js
@@ -1,3 +1,51 @@
+const playerChoice = "rock";
+const computerChoice = getComputerChoice();
+
+function playRound(playerChoice, computerChoice){
+    playerChoice = playerChoice.toLowerCase();
+
+    let msg = '';
+    let winLoseDraw;
+
+    switch(playerChoice){
+        case 'rock':
+            if(computerChoice === 'rock')
+            winLoseDraw = 3;
+            else if(computerChoice === 'paper')
+            winLoseDraw = 2;
+            else winLoseDraw = 1;
+            console.log('rock');
+            break;
+        case 'paper':
+            if(computerChoice === 'rock')
+            winLoseDraw = 1;
+            else if(computerChoice === 'paper')
+            winLoseDraw = 3;
+            else winLoseDraw = 2;
+            console.log('paper');
+            break;
+        case 'scissors':
+            if(computerChoice === 'rock')
+            winLoseDraw = 2;
+            else if(computerChoice === 'paper')
+            winLoseDraw = 1;
+            else winLoseDraw = 3;
+            console.log('scissos');
+            break;
+        default:
+            msg = 'Incorrect Choice!';
+            return;
+    }
+
+    if(winLoseDraw === 1)
+        msg = 'You Win! '+playerChoice+' beats '+computerChoice+'!';
+    else if(winLoseDraw === 2)
+        msg = 'You Lose! '+computerChoice+' beats '+playerChoice+'!';
+    else msg = 'Draw!';
+
+    return msg;
+}
+
 
 
 function getComputerChoice(){
